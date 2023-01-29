7f116731c4e3de34220db6b7d4417e5a2cf8cd34
Matias Baldanza <matiasbaldanza@gmail.com>
2023-01-29 02:27:48 -0300

    feat: Muestra las amortizaciones en una tabla, eliminando el console.table()

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const moneda = new Intl.NumberFormat('es-AR', {
 
 // elementos del DOM
 const prestamoForm = document.querySelector('[prestamo-form]');
+const amortizacionesSection = document.querySelector('[amortizaciones-section]');
 
 // event listeners
 prestamoForm.addEventListener('submit', simularPrestamo);
@@ -33,16 +34,16 @@ function simularPrestamo(event) {
     tasaAnual       =   parseFloat(document.querySelector('#interes').value);
     plazoEnMeses    =   parseInt(document.querySelector('#plazo').value);
 
-    const amortizaciones = calcularCuotas(montoTotal, plazoEnMeses, tasaAnual);
+    const amortizacion = calcularCuotas(montoTotal, plazoEnMeses, tasaAnual);
 
-    console.table(amortizaciones);
+    mostrarCuotas(amortizacion);
 
 }
 
 function calcularCuotas(montoTotal, plazoEnMeses, tasaAnual) {
     let tasaInteresMensual = (tasaAnual / 12) / 100;
     let cuotaMensual = montoTotal * (tasaInteresMensual / (1 - Math.pow(1 + tasaInteresMensual, -plazoEnMeses)));
-    let amortizaciones = [];
+    let amortizacion = [];
     
     for (let i = 0; i < plazoEnMeses; i++) {
         let interes = montoTotal * tasaInteresMensual;
@@ -50,17 +51,61 @@ function calcularCuotas(montoTotal, plazoEnMeses, tasaAnual) {
         let capital = cuotaMensual - interes;
         let saldo = montoTotal - capital;
     
-        amortizaciones.push({
-        "numero": i + 1,
-        "cuota": moneda.format(cuotaMensual),
-        "intereses": moneda.format(interes),
-        "impuestos": moneda.format(impuestos),
-        "capital": moneda.format(capital),
-        "saldo": moneda.format(saldo)
+        amortizacion.push({
+        "Número": i + 1,
+        "Cuota": moneda.format(cuotaMensual),
+        "Intereses": moneda.format(interes),
+        "Impuestos": moneda.format(impuestos),
+        "Capital": moneda.format(capital),
+        "Saldo": moneda.format(saldo)
         });
     
         montoTotal = saldo;
     }
     
-    return amortizaciones;
+    return amortizacion;
 }
+
+function mostrarCuotas(amortizacion) {
+    columnas = Object.keys(amortizacion[0]);
+
+    const tabla             =   document.createElement('table');
+    const cabeceraTabla     =   document.createElement('thead');
+    const cuerpoTabla       =   document.createElement('tbody');
+    const pieTabla          =   document.createElement('tfoot');
+    let fila                =   document.createElement('tr');  
+
+    // Construir encabezado de la tabla
+
+    fila = crearFila(columnas);
+    cabeceraTabla.appendChild(fila);
+
+    // Construir el cuerpo de la tabla
+
+    for (cuota of amortizacion) {
+        fila = crearFila(Object.values(cuota));
+        cuerpoTabla.appendChild(fila);
+    }
+
+
+    tabla.append(cabeceraTabla, cuerpoTabla);
+
+    amortizacionesSection.appendChild(tabla);
+}
+
+function crearFila(columnas) {
+    let fila =  document.createElement('tr');  
+    
+    for (columna of columnas) {
+        let celda = document.createElement('td');
+        let texto = columna;
+        let textoCelda = document.createTextNode(texto);
+
+        celda.appendChild(textoCelda);
+        fila.appendChild(celda);
+    }
+    return fila;
+}
+
+
+
