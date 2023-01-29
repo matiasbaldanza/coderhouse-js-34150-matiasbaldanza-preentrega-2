3f89ece0980de958c8d1cd71b2b5bcb5b93f28ef
Matias Baldanza <matiasbaldanza@gmail.com>
2023-01-29 01:29:08 -0300

    feat: Calcula amortizaciones de cuotas préstamo y las muestra en la consola del navegador

--- index.js
+++ index.js
@@ -10,30 +10,57 @@
     Sitio:      https://matiasbaldanza.github.io/coderhouse-js-34150-matiasbaldanza-preentrega-2/
 */
 
+// GLOBALS
+
 const IVA = 0.21;
+const moneda = new Intl.NumberFormat('es-AR', {
+    style: 'currency',
+    currency: 'ARS'
+});
 
 // elementos del DOM
 const prestamoForm = document.querySelector('[prestamo-form]');
-console.log(prestamoForm)
 
 // event listeners
 prestamoForm.addEventListener('submit', simularPrestamo);
 
+// MAIN
 
 function simularPrestamo(event) {
     event.preventDefault();
-    const prestamo = tomarDatosPrestamo();
 
+    montoTotal      =   parseFloat(document.querySelector('#monto').value);
+    tasaAnual       =   parseFloat(document.querySelector('#interes').value);
+    plazoEnMeses    =   parseInt(document.querySelector('#plazo').value);
 
-    
+    const amortizaciones = calcularCuotas(montoTotal, plazoEnMeses, tasaAnual);
+
+    console.table(amortizaciones);
+
 }
 
-function tomarDatosPrestamo() {
-    const datosPrestamo = {
-        monto:          parseFloat(document.querySelector('#monto').value),
-        tasaAnual:      parseFloat(document.querySelector('#interes').value),
-        plazoEnMeses:   parseInt(document.querySelector('#plazo').value), 
+function calcularCuotas(montoTotal, plazoEnMeses, tasaAnual) {
+    let tasaInteresMensual = (tasaAnual / 12) / 100;
+    let cuotaMensual = montoTotal * (tasaInteresMensual / (1 - Math.pow(1 + tasaInteresMensual, -plazoEnMeses)));
+    let amortizaciones = [];
+    
+    for (let i = 0; i < plazoEnMeses; i++) {
+        let interes = montoTotal * tasaInteresMensual;
+        let impuestos = interes * IVA;
+        let capital = cuotaMensual - interes;
+        let saldo = montoTotal - capital;
+    
+        amortizaciones.push({
+        "numero": i + 1,
+        "cuota": moneda.format(cuotaMensual),
+        "intereses": moneda.format(interes),
+        "impuestos": moneda.format(impuestos),
+        "capital": moneda.format(capital),
+        "saldo": moneda.format(saldo)
+        });
+    
+        montoTotal = saldo;
     }
-
-    return datosPrestamo;
+    
+    return amortizaciones;
 }
